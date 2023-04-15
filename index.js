3ae8d7229a6053bd8308ca659f18d5007f597870
austinsipp <austin.sipp@gmail.com>
2023-04-15 14:17:50 -0700

    add dotenv file and package

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
+
+
+
+app.get('/',(req,res) => {
+    res.send('Hello World')
+})
+
+
+app.get('*', (req,res) => {
+    res.send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
