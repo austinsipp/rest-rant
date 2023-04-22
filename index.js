6f8ba0f1881fa6c4b7a24f1a6f045b80ebe9e9cd
austinsipp <austin.sipp@gmail.com>
2023-04-22 13:04:59 -0700

    add delete functionality

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const methodOverride = require('method-override')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.use('/places', require('./controllers/places'))
 
 
