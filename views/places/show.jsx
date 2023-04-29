bf685fed54b89e7657a13d286876c5ce0d8898e7
austinsipp <austin.sipp@gmail.com>
2023-04-29 13:28:56 -0700

    cosmetic changes to show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,17 +5,26 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <div>
-                <p className='text-center'>
-                    {data.place.cuisines}
-                </p>
-                <img src={data.place.pic} alt={data.place.name}></img>
-                
-                <p className='text-center'>
-                    Located in {data.place.city}, {data.place.state}
-                </p>
-            </div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h1>{data.place.name}</h1>
+                    <img src={data.place.pic} alt={data.place.name}></img>
+                    <p className='text-center'>
+                        Located in {data.place.city}, {data.place.state}
+                    </p>
+                </div>
+            
+            <div className="col-sm-6">
+                <h2>
+                Description
+                </h2>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
+            
             <div>
                 Rating
             </div>
@@ -29,9 +38,10 @@ function show (data) {
                 <button type="submit" className="btn btn-danger">
                 Delete
                 </button>
-            </form>     
+            </form>  
+            </div>   
 
-
+            </div>
           </main>
         </Def>
     )
