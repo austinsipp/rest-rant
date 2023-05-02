d7c4439c48c28f755e66c96cc3a6aebea7795bc3
austinsipp <austin.sipp@gmail.com>
2023-05-02 11:35:25 -0700

    implement rant and rave on show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = ( 
+        <h3 className="inactive">
+            No Comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! :(' : 'Rave! :)'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main>
@@ -24,12 +43,10 @@ function show (data) {
                 <h4>
                     Serving {data.place.cuisines}
                 </h4>
-            
+
             <div>
-                Rating
-            </div>
-            <div>
-                Comments
+                <h2>Comments</h2>
+                {comments}
             </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
